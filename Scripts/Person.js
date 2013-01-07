f1c6600b2b4b6cc0b750832b17ae58352b4dfe1f
Alexander Chernenko <alexanderchernenko@gmail.com>
2013-01-08 01:03:12 +0200

    initial commit

---
+++ Scripts/Person.js
@@ -0,0 +1,25 @@
+﻿var Class = function () {
+    var klass = function () {
+        this.init.apply(this, arguments);
+    }
+    klass.prototype.init = function () { }
+    return klass;
+}
+
+var Person = new Class;
+
+// Person.prototype.Age = 10;
+
+Person.prototype.init = function (name) {
+    this.BirthDate = Date();
+    alert("person initialized");
+    console.log("person " + name + " initialized. Age is " + this.BirthDate);
+}
+
+var World = new Class;
+World.prototype.persons = [];
+World.prototype.addPerson = 
+function (person) {
+    debugger;
+    this.persons.push(person);
+}
